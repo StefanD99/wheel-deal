f5b4aa2b4e97298e48d4b59b50f2e36c0da3ecf3
StefanD99 <sdimitrijevic762@gmail.com>
2024-07-02 16:59:27 +0200

    Refactored code

--- src/fbConfig.js
+++ src/fbConfig.js
@@ -9,12 +9,12 @@ import {getStorage} from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID
+  apiKey: "AIzaSyC-_FM9PiDGFN8OdJwir55nRXXFZzf6azY",
+  authDomain: "wheel-deal-app-3a1b3.firebaseapp.com",
+  projectId: "wheel-deal-app-3a1b3",
+  storageBucket: "wheel-deal-app-3a1b3.appspot.com",
+  messagingSenderId: "756388902664",
+  appId: "1:756388902664:web:0fa44191d2654ce4cadebd"
 //   databaseUrl: process.env.REACT_APP_DATABASE_URL
 };
 
